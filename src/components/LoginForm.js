0f5cb81678093a9d008f7f7416666d48ae1a2162
KingslayerHn <lesterarte@gmail.com>
2021-09-25 01:27:42 -0600

    reestructure sign in done

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
-import { Card, Button, Form } from 'react-bootstrap';
 import styles from '../styles.module.scss';
-import bugdet from '../assets/budget.svg';
 import { useHistory } from 'react-router-dom';
 import { useForm } from '../hooks/useForm';
 import { setAlert } from '../redux/actions/alerts.action';
 import { useDispatch, useSelector } from 'react-redux';
-import Alert from './Alert';
 import Loader from './Loader';
 import { login } from '../redux/actions/auth.action';
+import Carousel from './login/Carousel';
+import { AiOutlineMail, RiLockPasswordLine } from 'react-icons/all';
+import {
+  Row,
+  Col,
+  Container,
+  Form,
+  Button,
+  InputGroup,
+  FormControl,
+} from 'react-bootstrap';
+import Alert from '../components/Alert';
 
 const LoginForm = () => {
   const history = useHistory();
@@ -47,59 +56,91 @@ const LoginForm = () => {
     history.push('/register');
   };
   return (
-    <div className={styles.banner}>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'Column ',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <h1>
-          <b>Budget App</b>
-        </h1>
-        <img src={bugdet} alt="budget" />
-      </div>
-      <Card style={{ width: 400 }}>
-        <Card.Body>
-          <Form className={styles.login} onSubmit={handleSubmit}>
-            <div>
-              <label>Username</label>
-              <input
-                autoComplete="false"
-                type="text"
-                name="email"
-                value={email}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <label>Password</label>
-              <input
-                autoComplete="false"
-                type="password"
-                name="password"
-                value={password}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className={styles.buttons}>
-              <Button
-                type="submit"
-                className={styles.primary}
-                onClick={handleSubmit}
-                disabled={loader}
+    <div>
+      <Row style={{ backgroundColor: '#fff', width: '70vw', height: '75vh' }}>
+        <Col xs={6} style={{ position: 'relative' }}>
+          <Container
+            style={{
+              display: 'flex',
+              height: '100%',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Form className={styles.login} onSubmit={handleSubmit}>
+              <h1>Welcome!!</h1>
+              <h3>Sign in to your account</h3>
+              <InputGroup
+                style={{
+                  boxShadow: '2px 2px 17px -10px rgba(0,0,0,0.75)',
+                  borderRadius: '20px',
+                }}
               >
-                {loader ? <Loader /> : 'Login'}
-              </Button>
-              <Button className={styles.secondary} onClick={handleChangePage}>
-                Register
-              </Button>
-            </div>
-          </Form>
-        </Card.Body>
-      </Card>
+                <InputGroup.Text className={styles.inputDescription}>
+                  <AiOutlineMail color="#0d56b3" />
+                </InputGroup.Text>
+                <FormControl
+                  autoComplete="false"
+                  type="email"
+                  placeholder="Email address"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                  name="email"
+                  value={email}
+                  onChange={handleInputChange}
+                />
+              </InputGroup>
+              <InputGroup
+                style={{
+                  boxShadow: '2px 2px 17px -10px rgba(0,0,0,0.75)',
+                  borderRadius: '20px',
+                  margin: 10,
+                }}
+              >
+                <InputGroup.Text className={styles.inputDescription}>
+                  <RiLockPasswordLine color="#0d56b3" />
+                </InputGroup.Text>
+                <FormControl
+                  placeholder="password"
+                  aria-label="password"
+                  aria-describedby="basic-addon1"
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handleInputChange}
+                />
+              </InputGroup>
+
+              <div className={styles.buttons}>
+                <Button
+                  type="submit"
+                  className={styles.primary}
+                  onClick={handleSubmit}
+                  disabled={loader}
+                >
+                  {loader ? <Loader /> : 'SIGN IN'}
+                </Button>
+                <Button className={styles.secondary} onClick={handleChangePage}>
+                  SIGN UP
+                </Button>
+              </div>
+            </Form>
+            <Alert />
+          </Container>
+        </Col>
+        <Col
+          xs={6}
+          style={{
+            maxWidth: '100%',
+            maxHeight: '100%',
+            padding: 0,
+            objectFit: 'cover',
+            overflow: 'hidden',
+          }}
+        >
+          <Carousel />
+        </Col>
+      </Row>
     </div>
   );
 };
